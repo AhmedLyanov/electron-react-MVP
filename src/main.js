c16ac0e2294a244d692c1a28992d8e71796c48f6
Ahmed <amoshal1997@gmail.com>
2025-01-19 15:29:27 +0300

    last commit

---
+++ src/main.js
@@ -0,0 +1,42 @@
+import { app, BrowserWindow, Menu } from 'electron';
+import path from 'node:path';
+import started from 'electron-squirrel-startup';
+
+if (started) {
+  app.quit();
+}
+
+const createWindow = () => {
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: MAIN_WINDOW_PRELOAD_WEBPACK_ENTRY,
+    },
+  });
+
+
+  Menu.setApplicationMenu(null);
+
+  
+  mainWindow.loadURL(MAIN_WINDOW_WEBPACK_ENTRY);
+
+
+  mainWindow.maximize();
+};
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
